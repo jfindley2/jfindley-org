78cd5967a0f9437c1b7e8708db90cfa682f71a35
Jacob Findley <jfindley2@cnm.edu>
2016-01-13 13:42:37 -0700

    Added some directories and milestone-1.php

---
+++ public_html/documentation/milestone-1.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8"/>
+		<title>Jacob Findley's homepage</title>
+	</head>
+	<body>
+		<h1>My Details</h1>
+		<p><strong>Purpose:</strong>I'm attempting to advertise myself.</p>
+		<p><strong>Audience:</strong>I intend to impress potential employers with my site.</p>
+		<p><strong>Goal:</strong> I will go into technical writing, using my site to put my name out there and provide
+		employers who investigate me a useful tool in knowing who I am.</p>
+
+		<h1>Persona</h1>
+		<p><strong>Name:</strong>Abigail Rhode</p>
+		<p><strong>Age:</strong> 39</p>
+		<p><strong>Job:</strong>Rhode has been a manager at ExampleTech for the past fourteen years.</p>
+		<p><strong>Goal:</strong>Rhode is, after a successful interview with a potential new employee, looking into
+		said interviewee's personal website.</p>
+		<p><strong>Behavior:</strong>Rhode is a confident and quietly affable woman who tends towards </p>
+
+
+
+	</body>
+</html>
