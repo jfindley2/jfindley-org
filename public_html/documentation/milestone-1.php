3bf4c9fdab7712fbeceae4348ce777341f0d3063
Jacob Findley <jfindley2@cnm.edu>
2016-01-20 08:04:55 -0700

    Pushing

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -8,15 +8,13 @@
 		<h1>My Details</h1>
 		<p><strong>Purpose:</strong>I'm attempting to advertise myself.</p>
 		<p><strong>Audience:</strong>I intend to impress potential employers with my site.</p>
-		<p><strong>Goal:</strong> I will go into technical writing, using my site to put my name out there and provide
-		employers who investigate me a useful tool in knowing who I am.</p>
+		<p><strong>Goal:</strong> I will go into technical writing, using my site to put my name out there and provide employers who investigate me a useful tool in knowing who I am.</p>
 
 		<h1>Persona</h1>
 		<p><strong>Name:</strong>Abigail Rhode</p>
 		<p><strong>Age:</strong> 39</p>
 		<p><strong>Job:</strong>Rhode has been a manager at ExampleTech for the past fourteen years.</p>
-		<p><strong>Goal:</strong>Rhode is, after a successful interview with a potential new employee, looking into
-			said interviewee's personal website.</p>
+		<p><strong>Goal:</strong>Rhode is, after a successful interview with a potential new employee, looking into said interviewee's personal website.</p>
 		<p><strong>Behavior:</strong>Rhode is a confident and quietly affable woman who tends towards moderately aggressive decisions, taking reasonable risks for reasonable rewards (She won't risk anything she isn't willing to lose). </p>
 
 		<h2><strong>Use Case</strong></h2>
@@ -24,6 +22,9 @@
 		<p><strong>Motivations:</strong>Rhode wishes to hire someone for ExampleTech.</p>
 		<p><strong>Time:</strong>An hour after my interview, at 2:00 PM.</p>
 		<p><strong>Location:</strong>In her office.</p>
+		<p>Rhode accesses jfindley.org</p>
+		<p>jfindley.org presents her with my contact information, my photo, a few details about me, my accomplishments, my educational history, and what certifications and diplomas I have earned. </p>
+		<p>Rhode reviews this information, taking into account the interview and what she wants in an employee.</p>
 
 
 
