a459788c7d8fe2e6f3eb5114d81f23a89f831fdf
Jacob Findley <jfindley2@cnm.edu>
2016-01-25 09:54:37 -0700

    Added images

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -15,7 +15,8 @@
 		<p><strong>Age:</strong> 39</p>
 		<p><strong>Job:</strong>Rhode has been a manager at ExampleTech for the past fourteen years.</p>
 		<p><strong>Goal:</strong>Rhode is, after a successful interview with a potential new employee, looking into said interviewee's personal website.</p>
-		<p><strong>Behavior:</strong>Rhode is a confident and quietly affable woman who tends towards moderately aggressive decisions, taking reasonable risks for reasonable rewards (She won't risk anything she isn't willing to lose). </p>
+		<p><strong>Behavior:</strong>Rhode is a confident and quietly affable woman who tends towards moderately aggressive decisions, taking reasonable risks for reasonable rewards (She won't risk anything she isn't willing to lose). She has been married for ten years, and has a five year old son. Because her retirement plan is better than that of her husband, she is the primary breadwinner of the household, and her husband spends much of his time at home.
+		She gets along with her employees and coworkers, though at a distance, and at best they could be described as acquaintances. She has a reputation of having a nasty if often restrained temper, and her pride makes it difficult for her to apologise. She keeps her emails very well organised. She is an extrovert with a good ability to read people.</p>
 
 		<h2><strong>Use Case</strong></h2>
 		<p><strong>Goals:</strong>Rhode is learning more information to ascertain whether or not I am employable.</p>
@@ -25,7 +26,8 @@
 		<p>Rhode accesses jfindley.org</p>
 		<p>jfindley.org presents her with my contact information, my photo, a few details about me, my accomplishments, my educational history, and what certifications and diplomas I have earned. </p>
 		<p>Rhode reviews this information, taking into account the interview and what she wants in an employee.</p>
-
+	<img src="pwp-jacob.svg">
+		<img src="jacobFindleyMobileSite.svg">
 
 
 	</body>
