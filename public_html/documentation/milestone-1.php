e93d41850af83133959164464cd3623951f7d08d
Jacob Findley <jfindley2@cnm.edu>
2016-01-25 13:14:11 -0700

    Added images

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -32,9 +32,9 @@
 			The viewer wishes to see my credentials and information. To do that, they go to my website and see said credentials and information.</p>
 		<p>The viewer wishes to see my contact information. To do that, they go to my website and see said contact information.</p><br/>
 		<p><strong>Sitemap:</strong> I currently intend on there being just one page.</p>
-			<p>First I will have relevant information about myself. Where I went to school, what qualifications I have, what I have achieved, any certifications I have, et cetera.</p>
-			<p>Then, I will have my contact information. Primarily, email.</p>
-			<p>There will be an image. Possibly multiple images, which loop over time?</p>
+		<p>First I will have relevant information about myself. Where I went to school, what qualifications I have, what I have achieved, any certifications I have, et cetera.</p>
+		<p>Then, I will have my contact information. Primarily, email.</p>
+		<p>There will be an image. Possibly multiple images, which loop over time?</p>
 		<img src="pwp-Jacob.svg">
 		<br/>
 		<img src="jfindleyMobilePage.svg">
