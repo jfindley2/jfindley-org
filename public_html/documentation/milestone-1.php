d36e9a1a4b9f6789d82900068bb77338037734c5
Jacob Findley <jfindley2@cnm.edu>
2016-01-25 13:11:33 -0700

    Added images

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -30,7 +30,12 @@
 		<br/>
 		<p><strong>Wireframe:</strong>
 			The viewer wishes to see my credentials and information. To do that, they go to my website and see said credentials and information.
-		The viewer wishes to see my contact information. To do that, they go to my website and see said contact information.</p>
+		The viewer wishes to see my contact information. To do that, they go to my website and see said contact information.</p><br/>
+		<p><strong>Sitemap:</strong> I currently intend on there being just one page.
+			First I will have relevant information about myself. Where I went to school, what qualifications I have, what I have achieved, any certifications I have, et cetera.
+			Then, I will have my contact information. Primarily, email.
+			There will be an image. Possibly multiple images, which loop over time?
+		</p>
 		<img src="pwp-Jacob.svg">
 		<br/>
 		<img src="jfindleyMobilePage.svg">
