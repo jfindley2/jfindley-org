c3d8bd9d559471f05c03349af8b7ea20fd45e5cc
Jacob Findley <jfindley2@cnm.edu>
2016-01-28 09:13:41 -0700

    Accidentally had the code for milestone-2 in milestone-1. Fixed.

---
+++ public_html/documentation/milestone-2.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+	<html lang="en">
+	<head>
+		<meta charset="UTF-8"/>
+		<title>Milestone 2</title>
+	</head>
+	<body>
+		<p>
+			The viewer wishes to see my credentials and information. To do that, they go to my website and see said credentials and information.</p>
+		<p>The viewer wishes to see my contact information. To do that, they go to my website and see said contact information.</p><br/>
+		<p><strong>Sitemap:</strong> I currently intend on there being just one page.</p>
+		<p>First I will have relevant information about myself. Where I went to school, what qualifications I have, what I have achieved, any certifications I have, et cetera.</p>
+		<p>Then, I will have my contact information. Primarily, email.</p>
+		<p>There will be an image. Possibly multiple images, which loop over time?</p>
+		<img src="pwp-Jacob.svg">
+		<br/>
+		<img src="jfindleyMobilePage.svg">
+	</body>
+</html>
