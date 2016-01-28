a35147ccaf698ac0321f2a14c4e02f4778ff76f2
Jacob Findley <jfindley2@cnm.edu>
2016-01-28 10:38:23 -0700

    Updated milestone-2

--- public_html/documentation/milestone-2.php
+++ public_html/documentation/milestone-2.php
@@ -5,17 +5,21 @@
 		<title>Milestone 2</title>
 	</head>
 	<body>
+		<div>
 		<p><strong>Interaction flow</strong>
 			The viewer wishes to see my credentials and information. To do that, they go to my website and see said credentials and information.</p>
 		<p>The viewer wishes to see my contact information. To do that, they go to my website and see said contact information.</p><br/>
 		<p><strong>Sitemap:</strong> I currently intend on there being just one page.</p>
 		<p>First I will have relevant information about myself. Where I went to school, what qualifications I have, what I have achieved, any certifications I have, et cetera.</p>
 		<p>Then, I will have my contact information. Primarily, email.</p>
-		<p>There will be an image. Possibly multiple images, which loop over time?</p>
+		<p>There will be an image. Possibly multiple images, which loop over time? This will not be on the mobile version.</p>
 		<p><strong>Wireframe:</strong></p>
-		<img src="pwp-Jacob.svg">
-		<br/>
+			<h2>Desktop</h2>
+			<img src="pwp-Jacob.svg">
+		<br/><br/>
+			<h2>Mobile</h2>
 		<img src="jfindleyMobilePage.svg">
-		<p><strong>Content Strategy: There will be a photo of myself or possibly something artistic. There will be a text description of myself and my history such that potential employers can know my accomplishments. There will be contact information such that potential employers can contact me.</strong></p>
+		<p><strong>Content Strategy:</strong> There will be a photo of myself or possibly something artistic. There will be a text description of myself and my history such that potential employers can know my accomplishments. There will be contact information such that potential employers can contact me. The mobile version of the site will not have a photo in consideration of size constraints.</p>
+			</div>
 	</body>
 </html>
