59b300efbbcb41160e4028f3468ddf1578f3ccc2
Jacob Findley <jfindley2@cnm.edu>
2016-02-02 11:48:36 -0700

    Added style.css and updated milestone-webpage.php

--- public_html/documentation/milestone-webpage.php
+++ public_html/documentation/milestone-webpage.php
@@ -2,8 +2,33 @@
 <html lang="en">
 	<head>
 		<meta charset="UTF-8"/>
-	</head>
-	<body>
+		<!-- Latest compiled and minified CSS -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
 
-	</body>
+		<!-- Optional theme -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css" integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r" crossorigin="anonymous">
+
+		<!-- Latest compiled and minified JavaScript -->
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+		<link rel="stylesheet" href="css/style.css"/>
+
+	</head>
+	<main>
+		<div class="container">
+			<div class="row">
+				<div class="col-md-12">
+					<h1>Title</h1>
+				</div>
+			</div><!-- class="row", the first row-->
+			<div class="row">
+				<div class="col-md-6">
+					<!-- Insert an image here-->
+				</div><!-- class="mol-md-6", the first column in the second row-->
+				<div class="col-md-6">
+
+				</div><!-- class="col-md-6", the second column in the second row-->
+			</div> <!-- class="row", the second row-->
+		</div> <!--class="container" -->
+
+	</main>
 </html>
